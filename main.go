81cc3f539fb04afbb9cb4af0d9d810e8cae2fb57
Josh Betz <j@joshbetz.com>
2021-08-21 15:19:41 -0500

    Refactor categorization algorithm
    Fulfill smallest groups first. So we can't have files fulfilling the
    yearly group before we're done with the monthly group, for example.
    
    In practice, this means something like -keep-monthly 12 -keep-yearly 2
    would keep the most recent 12 months and an additional 2 years.
    Previously this worked a little differently and it was more confusing to
    know exactly how many files you could end up with. This will be more
    precise.
    
    We've also added some more detail and colors to the stdout to make it
    easier to see what's happening.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
@@ -89,77 +90,99 @@ func main() {
 	})
 
 	// Process files
+	curDate := time.Now()
 	numRecent := 0
-
 	numDays := 0
-	curDay := time.Now()
-
 	numWeeks := 0
-	curWeek := time.Now()
-
 	numMonths := 0
-	curMonth := time.Now()
-
 	numYears := 0
-	curYear := time.Now()
 
 	if dryRunFlag {
 		fmt.Println("Dry run mode. Not deleting any files.")
 	}
 
 	for _, file := range files {
-		keep := false
 
 		// Check recent files
 		if numRecent < keepRecent {
-			keep = true
 			numRecent++
+			curDate = file.ModTime()
+
+			printKeeping(file, color.RedString("(Recent) "))
+			continue
 		}
 
 		// Check days
-		if numDays < keepDaily && file.ModTime().Before(curDay) {
-			keep = true
+		if numDays < keepDaily {
+			if file.ModTime().Before(curDate.AddDate(0, 0, -1)) {
+				numDays++
+				curDate = file.ModTime()
 
-			numDays++
-			curDay = curDay.AddDate(0, 0, -1)
+				printKeeping(file, color.YellowString("(Daily)  "))
+				continue
+			}
+
+			deleteFile(file)
+			continue
 		}
 
 		// Check weeks
-		if numWeeks < keepWeekly && file.ModTime().Before(curWeek) {
-			keep = true
+		if numWeeks < keepWeekly {
+			if file.ModTime().Before(curDate.AddDate(0, 0, -7)) {
+				numWeeks++
+				curDate = file.ModTime()
 
-			numWeeks++
-			curWeek = curWeek.AddDate(0, 0, -7)
+				printKeeping(file, color.CyanString("(Weekly) "))
+				continue
+			}
+
+			deleteFile(file)
+			continue
 		}
 
 		// Check months
-		if numMonths < keepMonthly && file.ModTime().Before(curMonth) {
-			keep = true
+		if numMonths < keepMonthly {
+			if file.ModTime().Before(curDate.AddDate(0, -1, 0)) {
+				numMonths++
+				curDate = file.ModTime()
 
-			numMonths++
-			curMonth = curMonth.AddDate(0, -1, 0)
+				printKeeping(file, color.BlueString("(Monthly)"))
+				continue
+			}
+
+			deleteFile(file)
+			continue
 		}
 
 		// Check years
-		if numYears < keepYearly && file.ModTime().Before(curYear) {
-			keep = true
+		if numYears < keepYearly {
+			if file.ModTime().Before(curDate.AddDate(-1, 0, 0)) {
+				numYears++
+				curDate = file.ModTime()
 
-			numYears++
-			curYear = curYear.AddDate(-1, 0, 0)
+				printKeeping(file, color.MagentaString("(Yearly) "))
+				continue
+			}
+
+			deleteFile(file)
+			continue
 		}
+	}
+}
 
-		if keep {
-			if verboseOneFlag || dryRunFlag {
-				fmt.Println("[", color.BlueString("Keeping"), " ]", file.Name(), file.ModTime())
-			}
-		} else {
-			if verboseTwoFlag {
-				fmt.Println("[", color.RedString("Deleting"), "]", file.Name(), file.ModTime())
-			}
+func printKeeping(file fs.FileInfo, detail string) {
+	if verboseOneFlag || dryRunFlag {
+		fmt.Println("[", "Keeping", detail, "]", file.Name(), file.ModTime())
+	}
+}
 
-			if !dryRunFlag {
-				os.Remove(backupDir + "/" + file.Name())
-			}
-		}
+func deleteFile(file fs.FileInfo) {
+	// Delete
+	if verboseTwoFlag {
+		fmt.Println("[", color.RedString("Deleting         "), "]", file.Name(), file.ModTime())
+	}
+
+	if !dryRunFlag {
+		os.Remove(backupDir + "/" + file.Name())
 	}
 }
